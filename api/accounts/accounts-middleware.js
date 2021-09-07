ff66be3ba2bff61eeb01314941679c7af588c58e
juliane1999 <lastjuliane@gmail.com>
2021-09-07 13:33:31 -0700

    semi-completed the routers

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -46,6 +46,9 @@ exports.checkAccountId = async (req, res, next) => {
   const account = await Account.getById(req.params.id)
   if(!account) {
     next({status:404, message: 'not found'})
+  } else {
+    req.account = account
+    next()
   }
  } catch (err) {
    next (err)
